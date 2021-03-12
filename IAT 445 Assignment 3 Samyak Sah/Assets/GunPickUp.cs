3072737f54636cab94c7c38dc3cbe6fe14621d58
samyaksah <samyakbbps@gmail.com>
2021-03-11 21:35:35 -0800

    Added UI

--- IAT 445 Assignment 3 Samyak Sah/Assets/GunPickUp.cs
+++ IAT 445 Assignment 3 Samyak Sah/Assets/GunPickUp.cs
@@ -4,15 +4,115 @@ using UnityEngine;
 
 public class GunPickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Gun gunScript;
+    public Rigidbody rb;
+    public BoxCollider coll;
+    public Transform player, gunContainer, fpsCam;
+    public GameObject PickUI, DropUI;
+
+    public float pickUpRange, UIRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    public bool equipped;
+    public static bool slotFull;
+
+    private void Start()
     {
-        
+        //Setup
+        if (!equipped)
+        {
+            gunScript.enabled = false;
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+            PickUI.SetActive(false);
+            DropUI.SetActive(true);
+        }
+        if (equipped)
+        {
+            gunScript.enabled = true;
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotFull = true;
+            PickUI.SetActive(true);
+            DropUI.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Check if player is in range and "E" is pressed
+        Vector3 distanceToPlayer = player.position - transform.position;
+        if(!equipped && distanceToPlayer.magnitude <= UIRange)
+        {
+            PickUI.SetActive(true);
+            DropUI.SetActive(false);
+        }
+        else
+        {
+            PickUI.SetActive(false);
+            DropUI.SetActive(false);
+        }
+
+        if (equipped)
+        {
+            DropUI.SetActive(true);
+            PickUI.SetActive(false);
+        }
+        if (!equipped && distanceToPlayer.magnitude <= pickUpRange && Input.GetKeyDown(KeyCode.E) && !slotFull)
+        {            
+            PickUp();            
+        }
+
+        //Drop if equipped and "Q" is pressed
+        if (equipped && Input.GetKeyDown(KeyCode.Q)) Drop();
+    }
+
+    private void PickUp()
+    {
+        equipped = true;
+        slotFull = true;
+
+        //Make weapon a child of the camera and move it to default position
+        transform.SetParent(gunContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        //Make Rigidbody kinematic and BoxCollider a trigger
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+
+        //Enable script
+        gunScript.enabled = true;
+
+        DropUI.SetActive(true);
+
+
+    }
+
+    private void Drop()
+    {
+        equipped = false;
+        slotFull = false;
+
+        //Set parent to null
+        transform.SetParent(null);
+
+        //Make Rigidbody not kinematic and BoxCollider normal
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        //Gun carries momentum of player
+        rb.velocity = player.GetComponent<Rigidbody>().velocity;
+
+        //AddForce
+        rb.AddForce(fpsCam.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(fpsCam.up * dropUpwardForce, ForceMode.Impulse);
+        //Add random rotation
+        float random = Random.Range(-1f, 1f);
+        rb.AddTorque(new Vector3(random, random, random) * 10);
+
+        //Disable script
+        gunScript.enabled = false;
     }
 }
